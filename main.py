f35466967c49f7236d1d59e4b0477dd6b9c34ee5
Roman Shcheglov <roman.shcheglov@gmail.com>
2019-05-17 19:46:45 +0300

    Основный скрипт приложения

---
+++ main.py
@@ -0,0 +1,32 @@
+from threading import Thread
+import time
+import datetime
+import sys
+
+from kivy.app import App
+
+from sources.Dashboard import Dashboard
+
+# os.environ['KIVY_GL_BACKEND'] = 'gl'
+# os.environ['KIVY_WINDOW'] = 'egl_rpi'
+
+
+# Основной класс приложения
+class SMCDashboardApp(App):
+    def build(self):
+        # основная страница
+        dashboard = Dashboard()
+        return dashboard
+
+
+if __name__ == "__main__":
+    old_excepthook = sys.excepthook
+
+    def app_excepthook(exctype, value, traceback):
+        if exctype == KeyboardInterrupt:
+            print("Exception: Keyboard interrupt received")
+        else:
+            old_excepthook(exctype, value, traceback)
+    sys.excepthook = app_excepthook
+
+    SMCDashboardApp.run()
