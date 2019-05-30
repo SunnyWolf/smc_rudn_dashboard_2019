750b6d975ef57892d7773b6404e4c59c8efa1de8
Roman Shcheglov <roman.shcheglov@gmail.com>
2019-05-30 13:32:32 +0300

    Тестовая версия с SpeedometerView

--- sources/Dashboard.py
+++ sources/Dashboard.py
@@ -1,20 +1,34 @@
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
-from kivy.uix.image import Image
+from kivy.uix.slider import Slider
+from kivy.uix.anchorlayout import AnchorLayout
 
 from sources.widgets.speedometerview import SpeedometerView
 
 
 # Main screen
 class Dashboard(BoxLayout):
+    def set_speed(self, instance, value):
+        self.speed_view.set_speed_value(value)
+
+    def set_battery(self, instance, value):
+        self.speed_view.set_battery_percent(value)
+
     def __init__(self, **kwargs):
         super(Dashboard, self).__init__(**kwargs)
 
-        self.orientation = 'horizontal'
+        self.orientation = 'vertical'
 
-        self.add_widget(Widget())
+        self.s1 = Slider(size_hint=(1., 0.1), max=160)
+        self.s2 = Slider(size_hint=(1., 0.1), max=1.0)
 
-        self.speed_view = SpeedometerView()
-        self.add_widget(self.speed_view)
+        self.s1.bind(value=self.set_speed)
+        self.s2.bind(value=self.set_battery)
 
-        self.add_widget(Widget())
+        self.al = AnchorLayout(anchor_x='center', anchor_y='center')
+        self.speed_view = SpeedometerView(size_hint=(None, None), size=(400, 400))
+        self.al.add_widget(self.speed_view)
+
+        self.add_widget(self.s1)
+        self.add_widget(self.s2)
+        self.add_widget(self.al)
+
