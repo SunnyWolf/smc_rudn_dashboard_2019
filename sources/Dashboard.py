7a0a6a4dab4e4ade07452f8f5fbc8091eefba9dd
Roman Shcheglov <roman.shcheglov@gmail.com>
2019-06-05 14:52:53 +0300

    Новая версия SpeedometerView

--- sources/Dashboard.py
+++ sources/Dashboard.py
@@ -10,9 +10,6 @@ class Dashboard(BoxLayout):
     def set_speed(self, instance, value):
         self.speed_view.set_speed_value(value)
 
-    def set_battery(self, instance, value):
-        self.speed_view.set_battery_percent(value)
-
     def __init__(self, **kwargs):
         super(Dashboard, self).__init__(**kwargs)
 
@@ -22,9 +19,8 @@ class Dashboard(BoxLayout):
         self.s2 = Slider(size_hint=(1., 0.1), max=1.0)
 
         self.s1.bind(value=self.set_speed)
-        self.s2.bind(value=self.set_battery)
 
-        self.al = AnchorLayout(anchor_x='center', anchor_y='center')
+        self.al = AnchorLayout(anchor_x='left', anchor_y='center')
         self.speed_view = SpeedometerView(size_hint=(None, None), size=(400, 400))
         self.al.add_widget(self.speed_view)
 
--- sources/widgets/speedometerview.py
+++ sources/widgets/speedometerview.py
@@ -5,70 +5,88 @@ from kivy.properties import NumericProperty
 speedometerview_layout = '''
 <SpeedometerView@FloatLayout>:
     canvas:
-        Ellipse:
+        # Background
+        Rectangle:
             size: min(self.size), min(self.size)
             source: 'images/SpeedView.png'
             pos:
-                self.pos[0] + (self.width - min(self.size)) / 2,\
-                self.pos[1] + (self.height - min(self.size)) / 2
-                
-        Ellipse:
-            size: min(self.size), min(self.size)
-            source: 'images/SpeedView_BatteryGreen.png'
-            pos:
-                self.pos[0] + (self.width - min(self.size)) / 2,\
-                self.pos[1] + (self.height - min(self.size)) / 2
-            angle_start: -150
-            angle_end: -150 - (210 - 150) * self.bt_val
-            
-        Ellipse:
-            size: min(self.size), min(self.size)
-            source: 'images/SpeedView_BatteryRed.png' if self.bt_val < 0.20 else 'images/SpeedView_BatteryGreen.png'
-            pos:
-                self.pos[0] + (self.width - min(self.size)) / 2,\
-                self.pos[1] + (self.height - min(self.size)) / 2
-            angle_start: -150
-            angle_end: -150 - (210 - 150) * self.bt_val
-
-        PushMatrix
-        Rotate:
-            axis: 0,0,1
-            angle: -self.sp_ang_start - self.sp_ang_wide * self.sp_val / self.sp_val_max
-            origin: self.center
+                self.pos[0] + (self.width - min(self.size)) / 2.0,\
+                self.pos[1] + (self.height - min(self.size)) / 2.0
+        
+        # Straight line
+        Rectangle:
+            size: \
+                (min(self.size) / 2.0 * (self.sp_val - self.sp_ang_val_max)/(self.sp_lin_val_max - self.sp_ang_val_max), min(self.size)) \
+                if self.sp_val > self.sp_ang_val_max else \
+                (0, 0)
+            pos: \
+                self.pos[0] + (self.width - min(self.size)) / 2.0 + self.width / 2.0, \
+                self.pos[1] + (self.height - min(self.size)) / 2.0 + 0.5
+            source: 'images/SpeedView_SpeedBlur2.png'
+        
+        # Arc line
         Ellipse:
             size: min(self.size), min(self.size)
             source: 'images/SpeedView_SpeedBlur.png'
             pos: 
                 self.pos[0] + (self.width - min(self.size)) / 2,\
                 self.pos[1] + (self.height - min(self.size)) / 2
-            angle_start: -2 - self.sp_ang_wide * self.sp_val / self.sp_val_max
-            angle_end: 0
+            angle_start: self.sp_ang_start
+            angle_end: self.sp_ang_start + self.sp_ang_wide * self.sp_val / self.sp_ang_val_max
+        
+        # Arrow
+        PushMatrix
+        Rotate:
+            angle: \
+                -self.sp_ang_start - self.sp_ang_wide * self.sp_max / self.sp_ang_val_max \
+                if self.sp_max < self.sp_ang_val_max else 0
+            origin: self.center
+        Ellipse:
+            size: min(self.size), min(self.size)
+            source: 'images/SpeedView_Arrow.png'
+            pos:
+                self.pos[0] + (self.width - min(self.size)) / 2 \
+                if self.sp_max < self.sp_ang_val_max else \
+                self.pos[0] + (self.width - min(self.size)) / 2 + \
+                self.width / 2 * (self.sp_max - self.sp_ang_val_max)/(self.sp_lin_val_max - self.sp_ang_val_max),\
+                self.pos[1] + (self.height - min(self.size)) / 2
         PopMatrix
         
+    Label:
+        text: str(int(root.sp_val))
+        pos: root.pos[0], root.pos[1] + self.height * 0.05
+        size_hint: (None, None)
+        font_size: min(root.size) / 3.5
+        font_name: 'fonts/SysBoldItalic.ttf'
+        markup: True
+        size: root.size
+        
         ### DEBUG ONLY ###
-        # Color:
-        #     rgba: 1,0,0,0.1
-        # Rectangle
-        #     size: self.size
-        #     pos: self.pos
+        # canvas:       
+            # Color:
+            #     rgba: 1,0,0,0.1
+            # Rectangle
+            #     size: self.size
+            #     pos: self.pos
+        
 '''
 Builder.load_string(speedometerview_layout)
 
 
 class SpeedometerView(FloatLayout):
-    sp_val_max = 160.0
-    sp_ang_start = -120.0
-    sp_ang_end = 120.0
+    sp_ang_val_max = 120.0
+    sp_ang_start = -135.0
+    sp_ang_end = 0.0
     sp_ang_wide = sp_ang_end - sp_ang_start
 
+    sp_lin_val_max = 182.0
+
+    sp_max = NumericProperty(0.0)
     sp_val = NumericProperty(0.0)
-    bt_val = NumericProperty(0.0)
 
     def set_speed_value(self, value):
         self.sp_val = value
-
-    def set_battery_percent(self, value):
-        self.bt_val = value
+        self.sp_max = max(value, self.sp_max)
 
     def __init__(self, **kwargs):
         super(SpeedometerView, self).__init__(**kwargs)
