451f0c336394b31ac4cbac6fa8575247b7b0f2ec
Roman Shcheglov <roman.shcheglov@gmail.com>
2019-06-10 00:50:21 +0300

    Новая версия

--- sources/Dashboard.py
+++ sources/Dashboard.py
@@ -3,6 +3,7 @@ from kivy.uix.slider import Slider
 from kivy.uix.anchorlayout import AnchorLayout
 
 from sources.widgets.speedometerview import SpeedometerView
+from sources.widgets.currentview import CurrentView
 
 
 # Main screen
@@ -10,21 +11,27 @@ class Dashboard(BoxLayout):
     def set_speed(self, instance, value):
         self.speed_view.set_speed_value(value)
 
+    def set_current(self, instance, value):
+        self.current_view.set_current_value(value)
+
     def __init__(self, **kwargs):
         super(Dashboard, self).__init__(**kwargs)
 
         self.orientation = 'vertical'
 
         self.s1 = Slider(size_hint=(1., 0.1), max=160)
-        self.s2 = Slider(size_hint=(1., 0.1), max=1.0)
+        self.s2 = Slider(size_hint=(1., 0.1), min=-120, max=60, value=0)
 
         self.s1.bind(value=self.set_speed)
+        self.s2.bind(value=self.set_current)
 
-        self.al = AnchorLayout(anchor_x='left', anchor_y='center')
-        self.speed_view = SpeedometerView(size_hint=(None, None), size=(400, 400))
-        self.al.add_widget(self.speed_view)
+        self.bl = BoxLayout(orientation="horizontal")
+        self.speed_view = SpeedometerView()
+        self.current_view = CurrentView()
+        self.bl.add_widget(self.speed_view)
+        self.bl.add_widget(self.current_view)
 
         self.add_widget(self.s1)
         self.add_widget(self.s2)
-        self.add_widget(self.al)
+        self.add_widget(self.bl)
 
