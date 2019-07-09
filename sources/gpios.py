2985db0fd3392f6ce49b1a2e34dbd49566c7cba5
Roman Shcheglov <roman.shcheglov@gmail.com>
2019-07-09 11:17:19 +0300

    fixes

--- sources/gpios.py
+++ sources/gpios.py
@@ -18,17 +18,30 @@ class Gpios:
     def on_light_off(self):
         self.indicator_light.set(IndicatorLight.OFF)
 
+    def on_changing(self):
+        if self.b_light_side.value == 1:
+            if self.b_light_far == 1:
+                self.on_light_far()
+            elif self.b_light_close.value == 1:
+                self.on_light_close()
+            else:
+                self.on_light_side()
+        else:
+            self.on_light_off()
+
     def __init__(self, dashboard):
         self.indicator_light = dashboard.ids.light
 
         self.b_light_side = Button(26, pull_up=None, active_state=True, bounce_time=1)
-        self.b_light_side.when_pressed = self.on_light_side
-        self.b_light_side.when_released = self.on_light_off
+        self.b_light_side.when_pressed = self.on_changing
+        self.b_light_side.when_released = self.on_changing
 
         self.b_light_close = Button(12, pull_up=None, active_state=True, bounce_time=1)
-        self.b_light_close.when_activated = self.on_light_close
+        self.b_light_close.when_activated = self.on_changing
+        self.b_light_close.when_released = self.on_changing
 
         self.b_light_far = Button(16, pull_up=None, active_state=True, bounce_time=1)
-        self.b_light_far.when_activated = self.on_light_far
+        self.b_light_far.when_activated = self.on_changing
+        self.b_light_far.when_released = self.on_changing
 
         # self.fan = PWMOutputDevice()
