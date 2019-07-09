e625e18bed58b63feec81911f6853cd435677bca
Roman Shcheglov <roman.shcheglov@gmail.com>
2019-07-09 11:21:12 +0300

    fixes

--- sources/gpios.py
+++ sources/gpios.py
@@ -20,7 +20,7 @@ class Gpios:
 
     def on_changing(self):
         if self.b_light_side.value == 1:
-            if self.b_light_far == 1:
+            if self.b_light_far.value == 1:
                 self.on_light_far()
             elif self.b_light_close.value == 1:
                 self.on_light_close()
@@ -32,15 +32,15 @@ class Gpios:
     def __init__(self, dashboard):
         self.indicator_light = dashboard.ids.light
 
-        self.b_light_side = Button(26, pull_up=None, active_state=True, bounce_time=1)
+        self.b_light_side = Button(26, pull_up=None, active_state=True, bounce_time=0.1)
         self.b_light_side.when_pressed = self.on_changing
         self.b_light_side.when_released = self.on_changing
 
-        self.b_light_close = Button(12, pull_up=None, active_state=True, bounce_time=1)
+        self.b_light_close = Button(12, pull_up=None, active_state=True, bounce_time=0.1)
         self.b_light_close.when_activated = self.on_changing
         self.b_light_close.when_released = self.on_changing
 
-        self.b_light_far = Button(16, pull_up=None, active_state=True, bounce_time=1)
+        self.b_light_far = Button(16, pull_up=None, active_state=True, bounce_time=0.1)
         self.b_light_far.when_activated = self.on_changing
         self.b_light_far.when_released = self.on_changing
 
