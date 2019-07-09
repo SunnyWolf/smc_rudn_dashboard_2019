13d13e4529682907a498966a42cf1687071da292
Roman Shcheglov <roman.shcheglov@gmail.com>
2019-07-09 15:10:52 +0300

    testing turn lights

--- sources/widgets/turnlight.py
+++ sources/widgets/turnlight.py
@@ -15,16 +15,16 @@ Builder.load_string(turnlight_layout)
 
 
 class TurnLight(Image):
+    anim = Animation(opacity=1.0, d=0.5) + Animation(opacity=0.0, d=0.5)
+    anim.repeat = True
+
     def __init__(self, **kwargs):
         super(TurnLight, self).__init__(**kwargs)
 
     def on(self):
-        self.anim = Animation(opacity=1.0, d=0.5) + Animation(opacity=0.0, d=0.5)
-        self.anim.repeat = True
         self.anim.start(self)
 
     def off(self):
-        if self.anim is not None:
-            self.anim.stop(self)
+        self.anim.stop(self)
         self.opacity = 0.0
 
